2735a7911fdd895efdff4e901ea63b673e305ca0
Devan Kavalchek <devankav7@gmail.com>
2022-03-21 22:44:26 -0400

    updated containers

--- mativ/lib/model/containers/equation_container.dart
+++ mativ/lib/model/containers/equation_container.dart
@@ -2,18 +2,20 @@ import 'package:mativ/model/operators/operable.dart';
 import 'package:mativ/model/operators/operator.dart';
 
 abstract class EquationContainer extends Operable {
+  Operable _child;
   EquationContainer? _left;
   EquationContainer? _right;
 
-  EquationContainer(
-      Operator operator, EquationContainer? left, EquationContainer? right)
-      : _left = left,
+  EquationContainer(Operator operator, Operable child, EquationContainer? left,
+      EquationContainer? right)
+      : _child = child,
+        _left = left,
         _right = right,
         super(operator);
 
-    void setLeft(EquationContainer? left) => _left = left;
-    void setRight(EquationContainer? right) => _right = right;
+  void setLeft(EquationContainer? left) => _left = left;
+  void setRight(EquationContainer? right) => _right = right;
 
-    EquationContainer? getLeft() => _left;
-    EquationContainer? getRight() => _right;
+  EquationContainer? getLeft() => _left;
+  EquationContainer? getRight() => _right;
 }
--- mativ/lib/model/containers/equation_group.dart
+++ mativ/lib/model/containers/equation_group.dart
@@ -1,8 +1,9 @@
 import 'package:mativ/model/containers/equation_container.dart';
+import 'package:mativ/model/operators/operable.dart';
 import 'package:mativ/model/operators/operator.dart';
 
 class EquationGroup extends EquationContainer {
-  EquationGroup(
-      Operator operator, EquationContainer? left, EquationContainer? right)
-      : super(operator, left, right);
+  EquationGroup(Operator operator, Operable child, EquationContainer? left,
+      EquationContainer? right)
+      : super(operator, child, left, right);
 }
