2bc1b22e6c3ad7a8a15c1ebe6f630e7e6a3aeb02
Devan Kavalchek <devankav7@gmail.com>
2022-03-22 00:00:25 -0400

    updated variable symbol

--- mativ/lib/model/symbols/variable_symbol.dart
+++ mativ/lib/model/symbols/variable_symbol.dart
@@ -2,4 +2,9 @@ import 'package:mativ/model/operators/operator.dart';
 import 'package:mativ/model/symbols/equation_symbol.dart';
 
 class VariableSymbol extends EquationSymbol {
+  String _variable;
+
+  VariableSymbol(String variable) : _variable = variable;
+
+  void getString() => _variable;
 }
